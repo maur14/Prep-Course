1af6b1b3ba08d641d493c99ad8cd9559bc05274a
maur14 <mauricioventrice06@gmail.com>
2023-03-02 19:36:31 -0300

    segundo cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -43,7 +43,7 @@ function suma(x, y) {
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
- return x - y ; 
+  return x - y ; 
 }
 
 function multiplica(x, y) {
@@ -62,7 +62,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
- return x == y ;
+  return x == y ;
 }
 
 function tienenMismaLongitud(str1, str2) {
@@ -101,11 +101,11 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
- if (num% 2 === 0) {
+  if (num% 2 === 0) {
   return true 
- } else{
+  } else{
   return false
- }
+  }
 }
 
 function esImpar(num) {
